2d5c66ddd46def4d3f429383aef14163a8fbfe3e
Kristin <kristinfaye.asuanobrien@gmail.com>
2021-11-01 18:36:20 -0500

    commit tree a988fe7697c4b5c73d32a556e6070649dadb3394

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
+  let result
+  if (feet <= 400){
+    result = 'This one is on me!';
+  }
+    if (feet >= 2000){
+     result = 'I will gladly take your thirty bucks.';
+    }
+    if (feet >= 2500){
+      result = 'No can do.';
+    }
+
+    return result
+  }
+      
+    
+    
+  // Write your code here!
+
+
+function ternaryCheckCity(isNewYorkCity){
+ return (isNewYorkCity === 'NYC'? 'Ok, sounds good.' : 'No go.');
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+        return 'Thank you.'
+        default: 
+        return 'Bye.'
 
-function switchOnCharmFromTip(){
+
+  }
   // Write your code here!
 }
